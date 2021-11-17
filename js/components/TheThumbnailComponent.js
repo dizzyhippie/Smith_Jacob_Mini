3f8dc41eed9bae76be48d664ce9bf96f62ba2c15
Jacob Smith <77408757+dizzyhippie@users.noreply.github.com>
2021-11-16 19:27:03 -0500

    Modal Functional and loading information

--- js/components/TheThumbnailComponent.js
+++ js/components/TheThumbnailComponent.js
@@ -1,12 +1,23 @@
 export default {
-    name: "TheModalComponent",
+    name: "TheThumbnailComponent",
 
     props: ["piece"],
 
+    methods: {
+        getMoreInfo(){
+            console.log("Getting more info!");
+            let modal = document.querySelector(".modal");
+            let closeButton = document.querySelector(".close");
+            modal.style.display = "block";
+            closeButton.addEventListener('click', () => {modal.style.display = "none";})
+        }
+    },
+
     template:`
-    <section class="thumbnail">
-    <img :src='"images/" + piece.pic' :alt="piece.name" class="selector-images">
-    <h1 class="thumbnail-title"> {{piece.title}} </h1>
-    <button class="learn-more">{{piece.cta}}</button>
+    <section class="thumbnail" @click="getMoreInfo">
+        <img :src='"images/" + piece.pic' :alt="piece.name" class="selector-images">
+        <h1 class="thumbnail-title">{{piece.title}}</h1>
+        <h2 class="thumbnail-price">{{piece.price}}</h2>
+        <button class="learn-more">{{piece.cta}}</button>
     </section>`
 }
